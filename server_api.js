be838215c6d72155f645a4cc9c04d4b4d19010c3
timpan <timpan.dev@gmail.com>
2021-05-21 15:27:21 +0800

    Take function as input to reduce unccessary codes

--- server_api.js
+++ server_api.js
@@ -14,17 +14,17 @@ console.log ("API Server started, listening to port 8888...");
 console.log ("Please access localhost:8888 for API calls.");
 
 app.get("/getAll", async function(req, res) {
-    await getAll().catch(console.error);
+    await connectDb(selectAll);
     res.send(results);
 });
 
 app.post("/insert", jsonParser, async function(req, res) {
     console.log('Received POST body: ', req.body);
-    register(req.body);
+    connectDb(insert, req.body);
 });
 
-
-async function getAll() {
+function connectDb(query) { connectDb(query, null); }
+async function connectDb(query, body) {
     var connection = mysql.createConnection(
         {
             host: 'localhost',
@@ -35,26 +35,7 @@ async function getAll() {
     );
     try {
         await connection.connect();
-        await query(connection);
-    } catch (e) {
-        console.error(e);
-    } finally {
-        connection.end();
-    }
-}
-
-async function register(obj){
-    var connection = mysql.createConnection(
-        {
-            host: 'localhost',
-            user: 'root',
-            password: '',
-            database: 'keywords_studios'
-        }
-    );
-    try {
-        await connection.connect();
-        await insert(connection, obj);
+        await query(connection, body);
     } catch (e) {
         console.error(e);
     } finally {
@@ -63,7 +44,7 @@ async function register(obj){
 }
 
 // Get all users in 'user' table
-function query(connection) {
+function selectAll(connection, obj) {
     connection.query('SELECT * FROM `user`', function(err, result, fields) {
         if(err) throw err;
         results = result;
